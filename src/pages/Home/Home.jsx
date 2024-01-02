bf2f2aa7c59871e6b46660e53824638097bf89e3
Yogeshwar-CM <cmyogeshwar@gmail.com>
2024-01-02 17:40:47 +0530

    Skeleton Structure of Home Page

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Home.css";
+import { Tilt } from "react-tilt";
+
+const Home = () => {
+  return (
+    <div className="Home">
+      <div className="nav">HOME ABOUT PROJECT ABOUT CONTACT</div>
+      <div className="main-grid">
+        <div className="item-1 item"></div>
+        <div className="item-2 item"></div>
+        <div className="item-3 item"></div>
+        <div className="item-4 item"></div>
+        <div className="item-5 item"></div>
+        <div className="item-6 item"></div>
+        <div className="item-7 item"></div>
+        <div className="item-8 item"></div>
+        <div className="item-9 item"></div>
+      </div>
+      <div className="foot"></div>
+    </div>
+  );
+};
+
+export default Home;
