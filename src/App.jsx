18cb69903bf0768424be1f42d7cdc2a8bf347ac5
Yogeshwar-CM <cmyogeshwar@gmail.com>
2024-01-03 11:03:41 +0530

    Mode Toggle Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home/Home";
 
 const App = () => {
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
   return (
-    <div className="App">
+    <div className={`App ${theme}`}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route
+            path="/"
+            element={<Home toggleTheme={toggleTheme} currentTheme={theme} />}
+          />
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./Home.css";
-import { Tilt } from "react-tilt";
+import Mode from "../../components/ModeButton/mode";
 
-const Home = () => {
+const Home = ({ toggleTheme, currentTheme }) => {
   return (
     <div className="Home">
       <div className="nav">HOME ABOUT PROJECT ABOUT CONTACT</div>
@@ -10,7 +10,9 @@ const Home = () => {
         <div className="item-1 item"></div>
         <div className="item-2 item"></div>
         <div className="item-3 item"></div>
-        <div className="item-4 item"></div>
+        <div className="item-4 item">
+        <Mode toggleTheme={toggleTheme} currentTheme={currentTheme} />
+        </div>
         <div className="item-5 item"></div>
         <div className="item-6 item"></div>
         <div className="item-7 item"></div>
