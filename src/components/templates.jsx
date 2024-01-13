fce17c21447c3817f00a544fec0c3443e1f0ce92
Yogeshwar-CM <cmyogeshwar@gmail.com>
2024-01-13 16:02:24 +0530

    1

---
+++ src/components/templates.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "./templates.css";
+
+const Templates = () => {
+  return (
+    <div className="Templates">
+      <h1>This is example H1 Text</h1>
+      <h2>This is example H2 Text</h2>
+      <h3>This is example H3 Text</h3>
+      <h4>This is example H4 Text</h4>
+      <p>Paragraph sample 1</p>
+      <p className="p-s">Paragraph sample 2</p>
+      <h2>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis et sequi
+        mollitia, eveniet non eos illum enim ipsum, quis iste quasi nulla ex
+        accusantium praesentium repudiandae beatae? Esse, nemo sed?
+      </h2>
+      <h4>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique
+        aliquid, incidunt veritatis eveniet beatae modi culpa ex corporis dolor,
+        sequi ratione ab excepturi delectus maiores ipsa, deserunt qui nisi
+        voluptate?
+      </h4>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam obcaecati
+        distinctio ratione eligendi molestias soluta rem nobis sequi sed
+        consectetur voluptatum explicabo totam hic inventore repellendus,
+        excepturi qui. Voluptatibus, adipisci!
+      </p>
+      <p className="p-s">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae debitis
+        labore accusamus. Nulla, quidem sint placeat aperiam possimus laborum
+        amet qui rem earum officia ab quas, nobis nemo dolorem magni?
+      </p>
+    </div>
+  );
+};
+
+export default Templates;
