5122d8ebe64a7f9073c1abb06384137fd4e8c808
Yogeshwar-CM <cmyogeshwar@gmail.com>
2024-06-24 18:56:36 +0530

    updates

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,36 @@
 import React, { useState } from "react";
 import profile from "../assets/profile.jpeg";
+import reactdark from "../assets/reactjsdark.svg";
+import awsdark from "../assets/awsdark.svg";
+import awsec2dark from "../assets/awsec2dark.svg";
+import bootstrapdark from "../assets/bootstrapdark.svg";
+import cppdark from "../assets/cppdark.svg";
+import cssdark from "../assets/cssdark.svg";
+import djangodark from "../assets/djangodark.svg";
+import dockerdark from "../assets/dockerdark.svg";
+import electrondark from "../assets/electrondark.svg";
+import figmadark from "../assets/figmadark.svg";
+import firebasedark from "../assets/firebasedark.svg";
+import gitdark from "../assets/gitdark.svg";
+import githubdark from "../assets/githubdark.svg";
+import herokudark from "../assets/herokudark.svg";
+import htmldark from "../assets/htmldark.svg";
+import jsdark from "../assets/jsdark.svg";
+import mongodbdark from "../assets/mongodbdark.svg";
+import muidark from "../assets/muidark.svg";
+import mysqldark from "../assets/mysqldark.svg";
+import netlifydark from "../assets/netlifydark.svg";
+import nodejsdark from "../assets/nodejsdark.svg";
+import npmdark from "../assets/npmdark.svg";
+import phpdark from "../assets/phpdark.svg";
+import postmandark from "../assets/postmandark.svg";
+import pythondark from "../assets/pythondark.svg";
+import reduxdark from "../assets/reduxdark.svg";
+import tailwinddark from "../assets/tailwinddark.svg";
+import vitedark from "../assets/vitedark.svg";
+
+import { motion } from "framer-motion";
 import "./Home.css";
-import { Link } from "react-router-dom";
 
 const Home = () => {
   const [rc, setRc] = useState(false);
@@ -95,8 +124,118 @@ const Home = () => {
       <div className="profile">
         <img src={profile} alt="profile picture" className="profile-pic" />
       </div>
-      <div className="mode">MODE</div>
-      <div className="skills">SKILLS</div>
+      <div className="mode">
+        <div className="switch">
+          <input type="checkbox" className="switch__input" id="Switch" />
+          <label className="switch__label" htmlFor="Switch">
+            <span className="switch__indicator"></span>
+            <span className="switch__decoration"></span>
+          </label>
+        </div>
+      </div>
+      <div className="skills">
+        <div className="scrollArea">
+          <div class="scroll">
+            <div class="m-scroll">
+              <span>
+                <img src={reactdark} />
+              </span>
+              <span>
+                <img src={awsdark} />
+              </span>
+              <span>
+                <img src={awsec2dark} />
+              </span>
+              <span>
+                <img src={bootstrapdark} />
+              </span>
+              <span>
+                <img src={cssdark} />
+              </span>
+              <span>
+                <img src={cppdark} />
+              </span>
+              <span>
+                <img src={djangodark} />
+              </span>
+              <span>
+                <img src={dockerdark} />
+              </span>
+              <span>
+                <img src={electrondark} />
+              </span>
+              <span>
+                <img src={tailwinddark} />
+              </span>
+              <span>
+                <img src={djangodark} />
+              </span>
+              <span>
+                <img src={figmadark} />
+              </span>
+              <span>
+                <img src={htmldark} />
+              </span>
+              <span>
+                <img src={gitdark} />
+              </span>
+            </div>
+          </div>
+          <div class="scroll">
+            <div class="l-scroll">
+              <span>
+                <img src={jsdark} />
+              </span>
+              <span>
+                <img src={herokudark} />
+              </span>
+              <span>
+                <img src={mongodbdark} />
+              </span>
+              <span>
+                <img src={githubdark} />
+              </span>
+              <span>
+                <img src={firebasedark} />
+              </span>
+              <span>
+                <img src={muidark} />
+              </span>
+              <span>
+                <img src={mysqldark} />
+              </span>
+              <span>
+                <img src={netlifydark} />
+              </span>
+              <span>
+                <img src={nodejsdark} />
+              </span>
+              <span>
+                <img src={npmdark} />
+              </span>
+              <span>
+                <img src={postmandark} />
+              </span>
+              <span>
+                <img src={phpdark} />
+              </span>
+              <span>
+                <img src={pythondark} />
+              </span>
+              <span>
+                <img src={reduxdark} />
+              </span>
+              <span>
+                <img src={vitedark} />
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="skills-btn">
+          <i className="fa-solid fa-arrow-right" id="skills-btn"></i>
+          Explore all skills
+        </div>
+      </div>
       <div className="resume">RESUME</div>
       <div className="projects">
         <h1 className="proj-title">PROJECTS</h1>
@@ -105,7 +244,7 @@ const Home = () => {
         </span>
       </div>
       <div className="contact">
-        <div className="">
+        <div className="contact-l">
           <div className="main-c">Need a Website?</div>
           <p className="contact-c">
             I accept freelance jobs, collaborations with startups / brands.
